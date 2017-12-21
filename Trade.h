04e829a0e810a22f12210a917d9e8a8b642b33e5
martinkoling <martin.koling@kcl.ac.uk>
2017-12-21 17:02:05 +0100

    asdas

--- Trade.h
+++ Trade.h
@@ -7,12 +7,32 @@
 #include <utility>
 #include <cstdint>
 
+
 // Do not add any extra #includes without asking on the KEATS discussion forum
 
-// TODO your code goes here:
+using std::accumulate;
+
+template<typename T>
+
+int bestProfit (T start , T end){
+
+    int startPrice = * start;
+
+    int toReturn = accumulate(start, end, 0, [&startPrice](int priceDifference, int & price){
+
+        int temp = price - startPrice;
+
+        if((temp) > priceDifference) priceDifference = temp;
+
+        if(price < startPrice) startPrice = price;
+
+        return priceDifference;
+    });
+
+    return toReturn;
 
 
-
+}
 
 
 
